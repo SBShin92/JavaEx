622e2773bc00744d21513f705cfa03ba5c976ad0
SBShin92 <aopaoi0987@gmail.com>
2024-03-26 12:23:00 +0900

    study oop

---
+++ src/com/javaex/oop/staticmember/StaticEx.java
@@ -0,0 +1,28 @@
+package com.javaex.oop.staticmember;
+
+public class StaticEx {
+	public static int refCount; // class variable
+	public static String classVar; // class variable
+	public String instanceVar; // instance variable
+	
+	/* 스태틱 블록으로 클래스 변수 초기화 */
+	static {
+		refCount = 0; // 스태틱 변수 초기화
+		classVar = "Static Member"; // 클래스 변수 접근
+//		instanceVar = "Instance Member"; // 인스턴스 변수로의 접근은 불가
+		System.out.println("초기 refCount: " + refCount);
+	}
+
+	public StaticEx() {
+		refCount++;
+		System.out.println("refcount: " + refCount);
+		System.out.println("Intstance 생성!");
+	}
+	
+	protected void finalize() throws Throwable {
+		refCount--;
+		
+		super.finalize();
+		System.out.println("소멸자 호출");
+	}
+}
---
+++ src/com/javaex/oop/staticmember/StaticExApp.java
@@ -0,0 +1,20 @@
+package com.javaex.oop.staticmember;
+
+public class StaticExApp {
+
+	public static void main(String[] args) {
+		StaticEx s1 = new StaticEx();
+		System.out.println("Main: refcount: " + s1.refCount);
+		
+		StaticEx s2 = new StaticEx();
+		System.out.println("Main: refcount: " + s2.refCount);
+		
+		StaticEx s3 = new StaticEx();
+		System.out.println("Main: refcount: " + StaticEx.refCount);
+		
+		s3 = null;
+		System.out.println("s3 해제");
+		System.out.println("refcount: " + StaticEx.refCount);
+	}
+	
+}
