b2006ceb6a98aedf2df8b92b9ba7cde33c4c1a2f
SBShin92 <aopaoi0987@gmail.com>
2024-03-26 15:53:26 +0900

    study oop
    Signed-off-by: sashin92 <aopaoi0987@naver.com>

--- src/com/javaex/oop/staticmember/Singleton.java
+++ src/com/javaex/oop/staticmember/Singleton.java
@@ -5,7 +5,12 @@ static 영역 활용
 단 하나의 인스턴스만 유지!
  */
 public class Singleton {
-    public Singleton() {
+    private static Singleton instance = new Singleton();
+    private Singleton() {
 
+    }
+
+    public static Singleton getInstance() {
+        return instance;
     }
 }
--- src/com/javaex/oop/staticmember/SingletonEx.java
+++ src/com/javaex/oop/staticmember/SingletonEx.java
@@ -1,4 +1,11 @@
 package com.javaex.oop.staticmember;
 
 public class SingletonEx {
+    public static void main(String[] args) {
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+
+        System.out.println("s1 = " + s1);
+        System.out.println("s2 = " + s2);
+    }
 }
