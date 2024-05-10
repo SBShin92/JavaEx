7ca42342bc230ba05ca5e1d5781832874298e5eb
Sangbeom Shin <aopaoi0987@gmail.com>
2024-05-10 15:13:54 +0900

    compression methods, update makeTable abstracts

--- src/com/javaex/jdbc/miniproject/PhoneBookDAOImplement.java
+++ src/com/javaex/jdbc/miniproject/PhoneBookDAOImplement.java
@@ -19,19 +19,35 @@ public class PhoneBookDAOImplement implements PhoneBookDAO {
     }
 
     @Override
+    public boolean checkIsPhoneBookTable() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean createPhoneBookTable() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean dropPhoneBookTable() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
     public List<PhoneBookVO> getList() {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
         List<PhoneBookVO> lst = new ArrayList<>();
         String sql = """
                     SELECT id, name, hp, tel
                     FROM phone_book
                 """;
-        try {
-            con = getConnection();
-            pstmt = con.prepareStatement(sql);
-            rs = pstmt.executeQuery();
+        try (
+            Connection con = getConnection();
+            PreparedStatement pstmt = con.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+        ) {
             while (rs.next()) {
                 PhoneBookVO tmp = new PhoneBookVO(rs.getLong("id"),
                         rs.getString("name"),
@@ -42,45 +58,22 @@ public class PhoneBookDAOImplement implements PhoneBookDAO {
         } catch (ClassNotFoundException e) {
             System.err.println("드라이버를 찾을 수 없습니다.");
         } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            if (con != null) {
-                try {
-                    con.close();
-                } catch (SQLException e) {
-                    System.err.println("Connection 객체를 닫는 데 실패했습니다.");
-                }
-            }
-            if (pstmt != null) {
-                try {
-                    pstmt.close();
-                } catch (SQLException e) {
-                    System.err.println("PreparedStatement 객체를 닫는 데 실패했습니다.");
-                }
-            }
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException e) {
-                    System.err.println("ResultSet 객체를 닫는 데 실패했습니다.");
-                }
-            }
+            System.err.println("SQL 에러");
         }
         return lst;
     }
 
     @Override
     public boolean insert(String name, String hp, String tel) {
-        Connection con = null;
-        PreparedStatement pstmt = null;
         int result = 0;
         String sql = """
                     INSERT INTO phone_book (id, name, hp, tel)
                     VALUES (seq_phone_book.NEXTVAL, ?, ?, ?)
                 """;
-        try {
-            con = getConnection();
-            pstmt = con.prepareStatement(sql);
+        try (
+            Connection con = getConnection();
+            PreparedStatement pstmt = con.prepareStatement(sql);
+        ) {
             pstmt.setString(1, name);
             pstmt.setString(2, hp);
             pstmt.setString(3, tel);
@@ -88,67 +81,34 @@ public class PhoneBookDAOImplement implements PhoneBookDAO {
         } catch (ClassNotFoundException e) {
             System.err.println("드라이버를 찾을 수 없습니다.");
         } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            if (con != null) {
-                try {
-                    con.close();
-                } catch (SQLException e) {
-                    System.err.println("Connection 객체를 닫는 데 실패했습니다.");
-                }
-            }
-            if (pstmt != null) {
-                try {
-                    pstmt.close();
-                } catch (SQLException e) {
-                    System.err.println("PreparedStatement 객체를 닫는 데 실패했습니다.");
-                }
-            }
+            System.err.println("SQL 에러");
         }
         return result == 1;
     }
 
     @Override
     public boolean delete(Long id) {
-        Connection con = null;
-        PreparedStatement pstmt = null;
         int result = 0;
         String sql = """
                     DELETE FROM phone_book
                     WHERE id = ?
                 """;
-        try {
-            con = getConnection();
-            pstmt = con.prepareStatement(sql);
+        try (
+            Connection con = getConnection();
+            PreparedStatement pstmt = con.prepareStatement(sql);
+        ) {
             pstmt.setLong(1, id);
             result = pstmt.executeUpdate();
         } catch (ClassNotFoundException e) {
             System.err.println("드라이버를 찾을 수 없습니다.");
         } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            if (con != null) {
-                try {
-                    con.close();
-                } catch (SQLException e) {
-                    System.err.println("Connection 객체를 닫는 데 실패했습니다.");
-                }
-            }
-            if (pstmt != null) {
-                try {
-                    pstmt.close();
-                } catch (SQLException e) {
-                    System.err.println("PreparedStatement 객체를 닫는 데 실패했습니다.");
-                }
-            }
+            System.err.println("SQL 에러");
         }
         return result == 1;
     }
 
     @Override
     public List<PhoneBookVO> search(String searchInputString) {
-        Connection con = null;
-        PreparedStatement pstmt = null;
         ResultSet rs = null;
         List<PhoneBookVO> lst = new ArrayList<>();
         String sql = """
@@ -157,9 +117,10 @@ public class PhoneBookDAOImplement implements PhoneBookDAO {
                     WHERE name LIKE ?
                 """;
 
-        try {
-            con = getConnection();
-            pstmt = con.prepareStatement(sql);
+        try (
+            Connection con = getConnection();
+            PreparedStatement pstmt = con.prepareStatement(sql);
+        ) {
             pstmt.setString(1, "%" + searchInputString + "%");
             rs = pstmt.executeQuery();
             while (rs.next()) {
@@ -172,22 +133,8 @@ public class PhoneBookDAOImplement implements PhoneBookDAO {
         } catch (ClassNotFoundException e) {
             System.err.println("드라이버를 찾을 수 없습니다.");
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.err.println("SQL 에러");
         } finally {
-            if (con != null) {
-                try {
-                    con.close();
-                } catch (SQLException e) {
-                    System.err.println("Connection 객체를 닫는 데 실패했습니다.");
-                }
-            }
-            if (pstmt != null) {
-                try {
-                    pstmt.close();
-                } catch (SQLException e) {
-                    System.err.println("PreparedStatement 객체를 닫는 데 실패했습니다.");
-                }
-            }
             if (rs != null) {
                 try {
                     rs.close();
@@ -198,5 +145,4 @@ public class PhoneBookDAOImplement implements PhoneBookDAO {
         }
         return lst;
     }
-
 }
